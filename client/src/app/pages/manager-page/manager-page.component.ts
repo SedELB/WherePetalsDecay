c99915a092892376fd2bd31fa9894d4dd361d73e
SedELB <saadoun500@gmail.com>
2026-01-19 12:18:26 -0500

    feat(page): Created the game list page with a Back button

---
+++ client/src/app/pages/manager-page/manager-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-manager-page',
+  imports: [RouterLink],
+  templateUrl: './manager-page.component.html',
+  styleUrl: './manager-page.component.scss',
+})
+export class ManagerPageComponent {
+
+}
