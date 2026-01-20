d38504e54ab43c0bf82c96c232e7531c614aa06d
SedELB <saadoun500@gmail.com>
2026-01-20 11:34:21 -0500

    style(adminpage) Changed the name of component to admin and changed the path to /admin

--- client/src/app/pages/admin-page/admin-page.component.spec.ts
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ManagerPageComponent } from './manager-page.component';
+import { ManagerPageComponent } from './admin-page.component';
 
 describe('ManagerPageComponent', () => {
   let component: ManagerPageComponent;
