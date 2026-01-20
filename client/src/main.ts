8117a1256f9507f5ba1aafc8ec7ae52520e7a6dd
Rayenskill <raybour9@gmail.com>
2026-01-20 15:01:46 -0500

    <style>: did some changes on the admin page css and html but needs to include components insted of raw html elements (TODO)

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,7 @@ import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { ManagerPageComponent } from '@app/pages/manager-page/manager-page.component';
+import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { environment } from './environments/environment';
 
@@ -19,7 +19,7 @@ const routes: Routes = [
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
-    { path: 'manager', component: ManagerPageComponent},
+    { path: 'admin', component: AdminPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
