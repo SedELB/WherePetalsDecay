d437c2f23cc0adef62ec1fdf570336b47f3af5ee
SedELB <saadoun500@gmail.com>
2026-01-20 11:34:21 -0500

    style(adminpage) Changed the name of component to admin and changed the path to /admin

---
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-page',
+  imports: [RouterLink],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.scss',
+})
+export class AdminPageComponent {
+
+}
