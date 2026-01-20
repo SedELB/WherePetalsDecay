055a4f5581081bbd8e8fd9bda795aa88d839553f
Rayenskill <raybour9@gmail.com>
2026-01-20 16:18:37 -0500

    <style>: Added game card component and fixed the admin page a little bit, also we need to create a component for the game list container

---
+++ client/src/app/components/game-card/game-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-card',
+  imports: [],
+  templateUrl: './game-card.component.html',
+  styleUrl: './game-card.component.scss',
+  standalone: true,
+})
+export class GameCardComponent {
+
+}
--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GameCardComponent } from '@app/components/game-card/game-card.component';
 
 @Component({
   selector: 'app-admin-page',
-  imports: [RouterLink],
+  imports: [RouterLink, GameCardComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
 })
