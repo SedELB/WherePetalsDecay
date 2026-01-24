69cd7fae4f4c2af395b78b6a96770306b32bb883
Rayenskill <raybour9@gmail.com>
2026-01-23 21:59:23 -0500

    feat(button-component and game-card-component): finisehd the button component for now (subject to changes in future) and adjusted game-card-component to fit the button component. We need to work more on the game-card-component to make it modular and start to work on the list-container-component

--- client/src/app/components/game-card/game-card.component.ts
+++ client/src/app/components/game-card/game-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@app/components/button/button.component';
 
 @Component({
   selector: 'app-game-card',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss',
   standalone: true,
---
+++ client/src/app/components/list-container/list-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-container',
+  imports: [],
+  templateUrl: './list-container.component.html',
+  styleUrl: './list-container.component.scss',
+})
+export class ListContainerComponent {
+
+}
