b0a6d2d5fd2768c2e3e9abba89a6ba843945e3ed
Rayenskill <raybour9@gmail.com>
2026-01-23 22:32:38 -0500

    chore(homepage): merged the homepage in this branch to be able to link the different pages (will merge back in dev)

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommunicationService } from '@app/services/communication.service';
+import { Message } from '@common/message';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-main-page',
@@ -8,16 +13,38 @@ import { RouterLink } from '@angular/router';
     imports: [RouterLink],
 })
 export class MainPageComponent {
-    readonly gameTitle: string = 'GrimStone';
+    readonly title: string = 'LOG2995';
+    message: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-    readonly teamNumber: string = '310';
+    constructor(private readonly communicationService: CommunicationService) {}
 
-    readonly teamMembers: string[] = [
-        'Aymene Adaouri',
-        'Aly Abdoulaye-Idriss',
-        'Rayen Bouriel',
-        'Saad El Bounjimi',
-        'Walid Benakmoum',
-        'Franck Fongang',
-    ];
+    sendTimeToServer(): void {
+        const newTimeMessage: Message = {
+            title: 'Hello from the client',
+            body: 'Time is : ' + new Date().toString(),
+        };
+        // Important de ne pas oublier "subscribe" ou l'appel ne sera jamais lancé puisque personne l'observe
+        this.communicationService.basicPost(newTimeMessage).subscribe({
+            next: (response) => {
+                const responseString = `Le serveur a reçu la requête a retourné un code ${response.status} : ${response.statusText}`;
+                this.message.next(responseString);
+            },
+            error: (err: HttpErrorResponse) => {
+                const responseString = `Le serveur ne répond pas et a retourné : ${err.message}`;
+                this.message.next(responseString);
+            },
+        });
+    }
+
+    getMessagesFromServer(): void {
+        this.communicationService
+            .basicGet()
+            // Cette étape transforme l'objet Message en un seul string
+            .pipe(
+                map((message: Message) => {
+                    return `${message.title} ${message.body}`;
+                }),
+            )
+            .subscribe(this.message);
+    }
 }
