b32f4a1c46f5bc3dbd0089846a9fd61d5c26b46b
Nakbe <walidd.benak@gmail.com>
2026-01-25 22:27:18 -0500

    <Feat>:Ajout de plusieurs style de bouton dependament de l'etat

--- client/src/app/components/button-component/button-component.component.ts
+++ client/src/app/components/button-component/button-component.component.ts
@@ -1,16 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button-component',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button-component.component.html',
   styleUrl: './button-component.component.scss',
 })
 export class ButtonComponentComponent {
   @Input() text: string = 'Button de Test ';
+  @Input() variant: 'default' | 'back' | 'save' = 'default';
+  @Input() disabled = false;
   @Output() clicked = new EventEmitter<void>();
 
   onClick() {
+    if (this.disabled) return;
     this.clicked.emit();
   }
 }
