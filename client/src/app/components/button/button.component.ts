58fc1d364936c341b8a0a9980d9fc95c4648af1c
Franck <franckfongang99@gmail.com>
2026-01-27 07:29:25 -0500

    refactor(game-creation): extract business logic into services and constants

--- client/src/app/components/button/button.component.ts
+++ client/src/app/components/button/button.component.ts
@@ -1,35 +1,33 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-button',
-  imports: [NgClass, RouterLink],
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.scss',
+    selector: 'app-button',
+    standalone: true,
+    imports: [NgClass, RouterLink],
+    templateUrl: './button.component.html',
+    styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+    @Input() color: string = 'white';
+    @Input() backgroundColor: string = '#95698D';
+    @Input() width?: number;
+    @Input() height?: number;
+    @Input() disabled: boolean = false;
+    @Input() backgroundPath?: string;
+    @Input() route?: string;
+    @Input() selected: boolean = false;
 
-  @Output() clicked = new EventEmitter<void>();
+    @Output() clicked = new EventEmitter<void>();
 
-  onClick(){
-    if (!this.disabled){
-      this.clicked.emit();
+    onClick() {
+        if (!this.disabled) {
+            this.clicked.emit();
+        }
     }
-  }
 
-  @Input() color: string = 'white';
-  @Input() backgroundColor: string = '#95698D';
-  @Input() width?: number;
-  @Input() height?: number;
-  @Input() disabled: boolean = false;
-  @Input() backgroundPath?: string;
-  @Input() route?: string;
-  @Input() selected: boolean = false;
-
-  // here is what we call a getter in Angular, it acts as an attribute that returns the return value in question
-  // For example : this.backgroundImage = "url(random/path)" if i define background path as "random/path"
-  get backgroundImage() {
-    return this.backgroundPath ? `url(${this.backgroundPath})` : null;
-  }
+    get backgroundImage() {
+        return this.backgroundPath ? `url(${this.backgroundPath})` : null;
+    }
 }
