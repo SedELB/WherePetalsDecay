4581810601caf95bddc2b482dd3208b66409465f
Franck <franckfongang99@gmail.com>
2026-01-27 11:28:09 -0500

    fix(test): fix game-card failing tests

--- client/src/app/components/game-card/game-card.component.ts
+++ client/src/app/components/game-card/game-card.component.ts
@@ -1,8 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-card',
-  imports: [],
+  imports: [NgClass],
+  standalone: true,
   templateUrl: './game-card.component.html',
   styleUrl: './game-card.component.scss',
   standalone: true,
