86198e7b0441784653b0c48f5b77428b6506cdde
Aymenead <aymene.adaouri@polymtl.ca>
2026-01-26 21:34:12 -0500

    feat: DTO validation for grid and gameSize, addGame and updateVisibility in game.service

--- server/app/model/dto/game/create-game.dto.ts
+++ server/app/model/dto/game/create-game.dto.ts
@@ -1,7 +1,29 @@
-import { IsString, MaxLength, IsBoolean, IsNumber } from 'class-validator';
+import { IsString, MaxLength, IsBoolean, IsNumber, IsEnum, IsObject, IsArray, ValidateNested, IsOptional} from 'class-validator';
 import { GAME_NAME_MAX_LENGTH } from './game.dto.constants';
-import { Tile } from '@app/model/schema/game.schema';
+import { Type } from 'class-transformer';
+// import { Tile } from '@app/model/schema/game.schema';
+import { GameMode, TileType, DoorState, TileItem } from '@app/model/schema/game.constants';
 
+export class TileDto {
+    @IsEnum(TileType)
+    type: TileType;
+
+    @IsOptional() // car on peut avoir une tuile sans item
+    @IsEnum(TileItem)
+    item?: TileItem | null;
+
+    @IsOptional()
+    @IsEnum(DoorState)
+    doorState?: DoorState;
+}
+
+class GameSizeDto {
+    @IsNumber()
+    rows: number;
+
+    @IsNumber()
+    cols: number;
+}
 
 export class CreateGameDto {
     @IsString()
@@ -9,23 +31,29 @@ export class CreateGameDto {
     name: string;
 
     @IsString()
-    size: object;
+    description: string;
 
-    @IsString()
-    gameMode: string;
+    @IsObject()
+    @ValidateNested() // Requis pour valider les propriétés internes de size
+    @Type(() => GameSizeDto)
+    size: GameSizeDto;
+
+    @IsEnum(GameMode)
+    gameMode: GameMode;
 
     @IsString()
     thumbnail: string;
 
+    @IsNumber()
+    maxPlayers: number;
+    
     // The grid validation will be in the Controller method.
-    grid: Tile[][];
+    @IsArray() // verifie que cest un tableau
+    @IsArray({ each: true }) // ... un tableau de tableaux
+    @ValidateNested({ each: true }) // entre dans TileDto et verifie selon les decorateurs
+    @Type(() => TileDto) // transforme JSON en instance de TileDto
+    grid: TileDto[][];
 
     @IsBoolean()
     isVisible: boolean;
-
-    @IsNumber()
-    maxPlayers: number;
-
-    @IsString()
-    description: string;
 }
