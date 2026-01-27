6f2b71ba54498560ca729cd2019ba46799c23cc3
Nakbe <walidd.benak@gmail.com>
2026-01-27 10:10:33 -0500

    Feat: Adding the name and the description input on the edition page of a game

--- client/src/app/pages/map-setup-page/map-setup-page.component.ts
+++ client/src/app/pages/map-setup-page/map-setup-page.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { ButtonComponentComponent } from '@app/components/button-component/button-component.component';
 
 @Component({
   selector: 'app-map-setup-page',
-  imports: [ButtonComponentComponent],
+  imports: [FormsModule, RouterLink, ButtonComponentComponent],
   templateUrl: './map-setup-page.component.html',
   styleUrl: './map-setup-page.component.scss',
 })
 export class MapSetupPageComponent {
+  private readonly router = inject(Router);
 
+  gameName = '';
+  gameDescription = '';
 
+  onBack(): void {
+    this.router.navigate(['/admin']);
+  }
 
-
-  onCreateMap() {
-    console.log('Carte créée et affichée sur le canvas !');
-
+  onSave(): void {
+    console.log('Sauvegarder (placeholder)');
   }
 }
-
-
