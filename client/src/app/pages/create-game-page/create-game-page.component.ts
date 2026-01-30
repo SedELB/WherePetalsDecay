0378a08eb7b5c4f315d73a0c0ca1e55579de4e05
Rayenskill <raybour9@gmail.com>
2026-01-30 14:30:46 -0500

    feat{create-game-interface): finished create-game inteface we need to implement the get method

--- client/src/app/pages/create-game-page/create-game-page.component.ts
+++ client/src/app/pages/create-game-page/create-game-page.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@app/components/button/button.component';
 
 @Component({
   selector: 'app-create-game-page',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './create-game-page.component.html',
   styleUrl: './create-game-page.component.scss',
 })
 export class CreateGamePageComponent {
+  
+  gameMode: string | null = null;
+  mapSize: string | null = null;
+
+
+  gameModeSelected(gameMode: string){
+    this.gameMode = gameMode;
+  }
+
+  mapSizeSelected(mapSize: string){
+    this.mapSize = mapSize;
+  }
+
+  get canCreateGame(){
+    return !!this.gameMode && !!this.mapSize;
+  }
 
 }
