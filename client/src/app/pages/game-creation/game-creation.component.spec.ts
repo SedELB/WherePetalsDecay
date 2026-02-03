8a9711fd280fe946d2fb8a31f9c889ffa3e2cf9b
Aly Abdoulaye-Idriss <alyabdoulaye72@gmail.com>
2026-02-03 14:07:46 -0500

    fix(lint): remove console statements, add trailing commas, extract magic numbers to constants

--- client/src/app/pages/game-creation/game-creation.component.spec.ts
+++ client/src/app/pages/game-creation/game-creation.component.spec.ts
@@ -4,6 +4,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GameCreationComponent } from './game-creation.component';
 import { GameService } from '@app/services/game.service';
 import { CharacterService } from '@app/services/character.service';
+import { BASE_STATS } from '@app/interfaces/character';
+
+const LIFE_WITH_BONUS = BASE_STATS.life + BASE_STATS.bonus;
+const SPEED_WITH_BONUS = BASE_STATS.speed + BASE_STATS.bonus;
+const TEST_AVATAR_INDEX = 5;
 
 describe('GameCreationComponent', () => {
     let component: GameCreationComponent;
@@ -38,12 +43,12 @@ describe('GameCreationComponent', () => {
 
     it('should have life value of 8 when life bonus is selected', () => {
         component.lifeBonusSelected = true;
-        expect(component.lifeValue).toBe(8);
+        expect(component.lifeValue).toBe(LIFE_WITH_BONUS);
     });
 
     it('should have speed value of 8 when speed bonus is selected', () => {
         component.lifeBonusSelected = false;
-        expect(component.speedValue).toBe(8);
+        expect(component.speedValue).toBe(SPEED_WITH_BONUS);
     });
 
     it('should have attack dice D6 when attackDiceD6 is true', () => {
@@ -60,7 +65,7 @@ describe('GameCreationComponent', () => {
 
     it('should reset form when going back to game selection', () => {
         component.characterName = 'Test';
-        component.selectedAvatarIndex = 5;
+        component.selectedAvatarIndex = TEST_AVATAR_INDEX;
         component.goBackToGameSelection();
         expect(component.characterName).toBe('');
         expect(component.selectedAvatarIndex).toBeNull();
@@ -69,13 +74,13 @@ describe('GameCreationComponent', () => {
 
     it('should validate form as invalid when name is empty', () => {
         component.characterName = '';
-        component.selectedAvatarIndex = 5;
+        component.selectedAvatarIndex = TEST_AVATAR_INDEX;
         expect(component.isFormValid()).toBe(false);
     });
 
     it('should validate form as valid when name and avatar are set', () => {
         component.characterName = 'Test';
-        component.selectedAvatarIndex = 5;
+        component.selectedAvatarIndex = TEST_AVATAR_INDEX;
         expect(component.isFormValid()).toBe(true);
     });
 });
