0aeec0e08c68e079e57616ce35efbd1695f7d61a
Rayen Bouriel <raybour9@gmail.com>
2026-02-04 23:54:31 +0000

    Revert "fix(tests, lint): fixed lint and tests temporarly"
    This reverts merge request !9

--- client/src/app/interfaces/tile.ts
+++
@@ -1,6 +0,0 @@
-import { TileTexture, TileItem } from '@common/enums';
-
-export interface Tile {
-    type: TileTexture;
-    item?: TileItem;
-}
--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -1,68 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Message } from '@common/message';
-import { Observable } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
+import { Message } from '@common/message';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { Game } from '@app/interfaces/game';
 
-type CreateGameDto = Omit<Game, '_id' | 'createdAt' | 'updatedAt' | '__v'>;
 @Injectable({
     providedIn: 'root',
 })
 export class CommunicationService {
-
     private readonly baseUrl: string = environment.serverUrl;
 
     constructor(private readonly http: HttpClient) {}
 
-    
-    // private readonly baseUrl: string = environment.serverUrl;
-
-    // constructor(private readonly http: HttpClient) {}
-
-    // basicGet(): Observable<Message> {
-    //     return this.http.get<Message>(`${this.baseUrl}/example`).pipe(catchError(this.handleError<Message>('basicGet')));
-    // }
-
-    // basicPost(message: Message): Observable<HttpResponse<string>> {
-    //     return this.http.post(`${this.baseUrl}/example/send`, message, { observe: 'response', responseType: 'text' });
-    // }
-
-    // private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
-    //     return () => of(result as T);
-    // }
-
-    getAllGames(): Observable<Game[]> {
-        return this.http.get<Game[]>(`${this.baseUrl}/game/games`);
+    basicGet(): Observable<Message> {
+        return this.http.get<Message>(`${this.baseUrl}/example`).pipe(catchError(this.handleError<Message>('basicGet')));
     }
 
-    getVisibleGames(): Observable<Game[]> {
-        return this.http.get<Game[]>(`${this.baseUrl}/game/visibleGames`);
+    basicPost(message: Message): Observable<HttpResponse<string>> {
+        return this.http.post(`${this.baseUrl}/example/send`, message, { observe: 'response', responseType: 'text' });
     }
 
-    deleteGame(id: string): Observable<void> {
-        return this.http.delete<void>(`${this.baseUrl}/game/${id}`);
+    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
+        return () => of(result as T);
     }
-
-    updateVisiblity(game: Game): Observable<void> {
-        return this.http.patch<void>(`${this.baseUrl}/game/delete/${game._id}`, !game.isVisible);
-    }
-
-    createGame(game: Game): Observable<void> {
-        const dto: CreateGameDto = {
-            name: game.name,
-            description: game.description,
-            size: game.size,
-            gameMode: game.gameMode,
-            thumbnail: game.thumbnail,
-            maxPlayers: game.maxPlayers,
-            grid: game.grid,
-            isVisible: game.isVisible,
-        };
-        return this.http.post<void>(`${this.baseUrl}/game/addGame`, dto);
-    }
-
-    // TODO: il manque le modify game
-
 }
