1b6b23ae3b36cfc195b8e17b41e187f11fa813d5
Rayenskill <raybour9@gmail.com>
2026-02-05 12:00:13 -0500

    chore(comments): deleting useless comments

--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Message } from '@common/message';
 import { Observable } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Game } from '@app/interfaces/game';
 
