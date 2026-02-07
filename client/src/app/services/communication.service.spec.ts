d26567641b1bc7abf2c65ee738781ef5d100589a
Rayenskill <raybour9@gmail.com>
2026-02-07 18:09:42 -0500

    chore(comments): Removed useless comments

--- client/src/app/services/communication.service.spec.ts
+++ client/src/app/services/communication.service.spec.ts
@@ -2,12 +2,10 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { CommunicationService } from '@app/services/communication.service';
-// import { Message } from '@common/message';
 
 describe('CommunicationService', () => {
     let httpMock: HttpTestingController;
     let service: CommunicationService;
-    // let baseUrl: string;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -16,7 +14,6 @@ describe('CommunicationService', () => {
         });
         service = TestBed.inject(CommunicationService);
         httpMock = TestBed.inject(HttpTestingController);
-        // baseUrl = service['baseUrl'];
     });
 
     afterEach(() => {
