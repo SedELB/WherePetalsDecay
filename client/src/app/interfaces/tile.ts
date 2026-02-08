7bee7f69c327d2352bd4f982219454b935727be8
Franck <franckfongang99@gmail.com>
2026-02-07 19:02:22 -0500

    fix(tuile): remove and edit tuile interface

--- client/src/app/interfaces/tile.ts
+++ client/src/app/interfaces/tile.ts
@@ -1,9 +1,1 @@
-import { Vec2 } from './vec2';
-
 export type TileType = 'floor' | 'wall' | 'water' | 'ice' | 'doorOpen' | 'doorClosed';
-
-export interface Tile {
-    position: Vec2;
-    type: TileType;
-    isClicked?: boolean;
-}
