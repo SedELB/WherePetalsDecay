22686a629aab7a20e2425ba37f91cb2c2b11ba85
Rayenskill <raybour9@gmail.com>
2026-02-10 10:05:46 -0500

    feat(waiting-room): Added the waiting-room

---
+++ client/src/app/pages/waiting-room/waiting-room.component.spec.ts
@@ -0,0 +1,24 @@
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { WaitingRoomComponent } from './waiting-room.component';
+
+describe('WaitingRoomComponent', () => {
+    let component: WaitingRoomComponent;
+    let fixture: ComponentFixture<WaitingRoomComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [WaitingRoomComponent],
+            providers: [provideRouter([])],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(WaitingRoomComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
