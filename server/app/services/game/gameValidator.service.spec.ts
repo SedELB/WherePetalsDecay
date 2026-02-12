843ceeee07ce8627dedb166a2cabc8e0946ab29a
Rayenskill <raybour9@gmail.com>
2026-02-11 22:15:30 -0500

    refactor(const): Added const message

--- server/app/services/game/gameValidator.service.spec.ts
+++ server/app/services/game/gameValidator.service.spec.ts
@@ -49,22 +49,22 @@ describe('GameValidator', () => {
 
     it('isTextLengthValid() should fail if the game name is empty', () => {
         const emptyNameGame = { ...getValidGame(), name: '' };
-        expect(() => gameValidatorService.isTextLengthValid(emptyNameGame)).toThrow('["Le champ nom est vide !"]');
+        expect(() => gameValidatorService.isTextLengthValid(emptyNameGame)).toThrow('Le champ nom est vide !');
     });
 
     it('isTextLengthValid() should fail if the game name exceeds max length', () => {
         const longNameGame = { ...getValidGame(), name: 'QWERTYUIOPASDFGHJKLZXCVBNM' };
-        expect(() => gameValidatorService.isTextLengthValid(longNameGame)).toThrow('["Le champ nom dépasse la longueur maximale !"]');
+        expect(() => gameValidatorService.isTextLengthValid(longNameGame)).toThrow('Le champ nom dépasse la longueur maximale !');
     });
 
     it('isTextLengthValid() should fail if the game desc is empty', () => {
         const emptyDescGame = { ...getValidGame(), description: '' };
-        expect(() => gameValidatorService.isTextLengthValid(emptyDescGame)).toThrow('["Le champ description est vide !"]');
+        expect(() => gameValidatorService.isTextLengthValid(emptyDescGame)).toThrow('Le champ description est vide !');
     });
 
     it('isTextLengthValid() should fail if the game desc is exceeds max length', () => {
         const longDescGame = { ...getValidGame(), description: 'a'.repeat(DESC_MAX_LENGTH + 1) };
-        expect(() => gameValidatorService.isTextLengthValid(longDescGame)).toThrow('["Le champ description dépasse la longueur maximale !"]');
+        expect(() => gameValidatorService.isTextLengthValid(longDescGame)).toThrow('Le champ description dépasse la longueur maximale !');
     });
 
     it('isTextLengthValid() should fail with multiple errors', () => {
