77e7a021e14d7e3a2cc292d0629878b0f64f53df
Rayenskill <raybour9@gmail.com>
2026-02-24 10:21:35 -0500

    feature(alert): Added confirmation when deleting a game and uniformisez description when hovering on tools in editor page

--- client/src/app/pages/join-game-page/join-game-page.component.spec.ts
+++ client/src/app/pages/join-game-page/join-game-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { JoinGamePageComponent } from './join-game-page.component';
 
 describe('JoinGamePageComponent', () => {
@@ -8,7 +7,7 @@ describe('JoinGamePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [JoinGamePageComponent]
+      imports: [JoinGamePageComponent],
     })
     .compileComponents();
 
