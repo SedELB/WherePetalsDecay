10b1ca399070b0c98cf6d8c8217a2ddf12ac4f8c
Rayenskill <raybour9@gmail.com>
2026-02-24 13:08:43 -0500

    feat(join):Activated joingame button

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '@app/components/button/button.component';
+import { ɵɵRouterLink } from "@angular/router/testing";
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './homepage.component.html',
     styleUrls: ['./homepage.component.scss'],
-    imports: [ButtonComponent],
+    imports: [ButtonComponent, ɵɵRouterLink],
 })
 export class HomePageComponent {
     readonly gameTitle: string = 'GrimStone';
