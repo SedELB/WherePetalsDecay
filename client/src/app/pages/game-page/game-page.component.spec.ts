fe57d552d90ec911a3cc953eeb9e79d2c67133d6
Aymenead <aymene.adaouri@polymtl.ca>
2026-03-09 12:48:00 -0400

    feat: added game-page component and game-view service & added getSocketId

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,23 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { GamePageComponent } from './game-page.component';
 
 describe('GamePageComponent', () => {
-  let component: GamePageComponent;
-  let fixture: ComponentFixture<GamePageComponent>;
+    let component: GamePageComponent;
+    let fixture: ComponentFixture<GamePageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GamePageComponent],
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [GamePageComponent],
+            providers: [provideRouter([])],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(GamePageComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+        fixture = TestBed.createComponent(GamePageComponent);
+        component = fixture.componentInstance;
+        await fixture.whenStable();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
---
+++ client/src/app/services/game-view/game-view.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, signal } from '@angular/core';
+import { WebSocketService } from '@app/services/web-socket/web-socket.service';
+import { SocketNamespace } from '@common/enums';
+import { JoinGameEvents } from '@common/join.gateway.events';
+import { Lobby } from '@common/lobby';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GameViewService {
+    private readonly namespace = SocketNamespace.Join;
+    readonly gameLobby = signal<Lobby | null>(null);
+
+    constructor(private readonly webSocketService: WebSocketService) {
+        this.setupWebSocketListeners();
+    }
+
+    private setupWebSocketListeners(): void {
+        this.webSocketService.onNamespace<Lobby>(this.namespace, JoinGameEvents.GameStarted, (lobby) => {
+            this.setGames(lobby);
+        });
+    }
+
+    // Emit
+    sendEndTurn(lobbyId: string): void {
+        this.webSocketService.emitNamespace(this.namespace, JoinGameEvents.EndTurn, lobbyId);
+    }
+
+    sendAbandon(lobbyId: string): void {
+        this.webSocketService.emitNamespace(this.namespace, JoinGameEvents.PlayerAbandon, lobbyId);
+    }
+
+    sendAction(lobbyId: string, action: null): void {
+        // TODO: replace null with actual action type
+        this.webSocketService.emitNamespace(this.namespace, JoinGameEvents.PlayerAction, { lobbyId, action });
+    }
+
+    // utils
+    setGames(gameLobby: Lobby): void {
+        this.gameLobby.set(gameLobby);
+    }
+
+    getLocalSocketId(): string | undefined {
+        return this.webSocketService.getSocketId(this.namespace);
+    }
+}
