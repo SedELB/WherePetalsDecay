3ec5f4cbb10d661b09302674669ff0e697a619b5
Franck <franckfongang99@gmail.com>
2026-03-16 16:12:51 -0400

    fix: prevent WASD movement while typing in chat, replace direct DOM manipulation with Angular methods, and split server modules

--- server/app/modules/lobby.module.ts
+++ server/app/modules/lobby.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { ChatGateway } from '@app/gateways/chat/chat.gateway';
+import { JoinGateway } from '@app/gateways/join/join.gateway';
+import { LobbyService } from '@app/services/lobby/lobby.service';
+import { Logger, Module, forwardRef } from '@nestjs/common';
+import { GameModule } from './game.module';
 
 @Module({
-  imports: [],
-  providers: [],
-  exports: [],
+  imports: [forwardRef(() => GameModule)],
+  providers: [JoinGateway, ChatGateway, LobbyService, Logger],
+  exports: [LobbyService],
 })
 export class LobbyModule {}
