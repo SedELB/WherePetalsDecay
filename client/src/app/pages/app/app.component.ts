5277e05f2c9ae407adc952e0ee0284fe8fcc7011
Franck <franckfongang99@gmail.com>
2026-03-18 16:17:19 -0400

    fix: added background image to local storage

--- client/src/app/pages/app/app.component.ts
+++ client/src/app/pages/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const BACKGROUND_STORAGE_KEY = 'cached-background-url';
+const BACKGROUND_EXPIRY_KEY = 'cached-background-expiry';
+const BACKGROUND_ASSET_PATH = '/assets/background.png';
+const ONE_DAY_MS = 86400000;
 
 @Component({
     selector: 'app-root',
@@ -7,4 +12,17 @@ import { RouterOutlet } from '@angular/router';
     styleUrls: ['./app.component.scss'],
     imports: [RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        const expiry = localStorage.getItem(BACKGROUND_EXPIRY_KEY);
+        const isExpired = !expiry || Date.now() > Number(expiry);
+
+        if (isExpired) {
+            localStorage.setItem(BACKGROUND_STORAGE_KEY, BACKGROUND_ASSET_PATH);
+            localStorage.setItem(BACKGROUND_EXPIRY_KEY, String(Date.now() + ONE_DAY_MS));
+        }
+
+        const backgroundUrl = localStorage.getItem(BACKGROUND_STORAGE_KEY) as string;
+        document.documentElement.style.setProperty('--bg-image', `url('${backgroundUrl}')`);
+    }
+}
