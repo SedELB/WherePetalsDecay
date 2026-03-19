78ee8fb426ed16fa8b3f3db807f9d1695ae3e8c4
Franck <franckfongang99@gmail.com>
2026-03-18 23:27:07 -0400

    fix: assets size, switch format to webp

---
+++ common/constants/asset-paths.constants.ts
@@ -0,0 +1,15 @@
+export const ASSET_PATHS = {
+    background: './assets/background.webp',
+    logo: './assets/GrimStoneLogo.webp',
+    tiles: {
+        floor: './assets/tiles/default.webp',
+        wall: './assets/tiles/wall.webp',
+        water: './assets/tiles/water.webp',
+        ice: './assets/tiles/ice.webp',
+        doorOpened: './assets/tiles/opened_door.webp',
+        doorClosed: './assets/tiles/closed_door.webp',
+    },
+    avatars: {
+        basePath: './assets/avatars',
+    },
+} as const;
