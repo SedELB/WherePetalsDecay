a478fb1d92ce60fa096582ed0b56a6b9587e7eac
SedELB <saadoun500@gmail.com>
2026-03-23 16:56:37 -0400

    feat: Implemented a basic diamond-shaped isometric view. Gameplay not working at this moment.

---
+++ client/src/app/components/isometric-map/isometric-map.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IsometricMapComponent } from './isometric-map.component';
+
+describe('IsometricMapComponent', () => {
+  let component: IsometricMapComponent;
+  let fixture: ComponentFixture<IsometricMapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IsometricMapComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IsometricMapComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
