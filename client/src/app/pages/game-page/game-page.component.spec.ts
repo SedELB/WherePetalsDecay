f920643d012f2eaf816d3884d77dbba7836ceb5b
Franck <franckfongang99@gmail.com>
2026-03-31 03:47:59 -0400

    fix: game page and waiting room tests

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -34,6 +34,7 @@ describe('GamePageComponent', () => {
 
     const createPlayer = (socketId: string, overrides: Partial<Player> = {}): Player => ({
         socketId, isHost: false, winsCount: 0, hasAbandonned: false,
+        combatCount: 0, lossCount: 0, totalHpLost: 0, totalHpDealt: 0, visitedTilesCount: 0,
         character: {
             name: `Player-${socketId}`, avatar: 'avatar.png', life: DEFAULT_LIFE, speed: 4,
             attack: 4, defense: 4, lifeBonus: false, attackDice: 'D6', defenseDice: 'D4',
