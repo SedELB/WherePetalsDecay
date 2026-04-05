241eda6fbf5347366d601893e6985d2399ff7805
Franck <franckfongang99@gmail.com>
2026-04-05 04:37:23 -0400

    feat: add loading component with transition and integrate in admin, create, and join pages

--- client/src/app/pages/join-game-page/join-game-page.component.ts
+++ client/src/app/pages/join-game-page/join-game-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '@app/components/button/button.component';
+import { LoadingComponent } from '@app/components/loading/loading.component';
 import { LobbyCardComponent } from '@app/components/lobby-card/lobby-card.component';
 import { ROUTES } from '@app/constants/routes.constants';
 import { WebSocketService } from '@app/services/web-socket/web-socket.service';
@@ -10,46 +11,40 @@ import { Lobby } from '@common/lobby';
 
 // The page after clicking "Joindre une partie"
 @Component({
-  selector: 'app-join-game-page',
-  imports: [ButtonComponent, LobbyCardComponent],
-  templateUrl: './join-game-page.component.html',
-  styleUrl: './join-game-page.component.scss',
+    selector: 'app-join-game-page',
+    imports: [ButtonComponent, LobbyCardComponent, LoadingComponent],
+    templateUrl: './join-game-page.component.html',
+    styleUrl: './join-game-page.component.scss',
 })
 export class JoinGamePageComponent implements OnInit, OnDestroy {
-  protected readonly buttonVariant = ButtonVariant;
-  private readonly webSocketService = inject(WebSocketService);
-  private readonly router = inject(Router);
-  readonly routes = ROUTES;
+    protected readonly buttonVariant = ButtonVariant;
+    private readonly webSocketService = inject(WebSocketService);
+    private readonly router = inject(Router);
+    readonly routes = ROUTES;
 
-  activeLobbies: Lobby[] = [];
+    activeLobbies: Lobby[] = [];
+    isLoading = true;
+    isReady = false;
 
-  ngOnInit(): void {
-    // Listener for updating available lobbies
-    this.webSocketService.onNamespace<Lobby[]>(
-      SocketNamespace.Join,
-      JoinGameEvents.UpdatedLobbiesList,
-      (availableLobbies) => {
-        this.activeLobbies = availableLobbies;
-      },
-    );
+    ngOnInit(): void {
+        this.webSocketService.onNamespace<Lobby[]>(SocketNamespace.Join, JoinGameEvents.UpdatedLobbiesList, (availableLobbies) => {
+            this.activeLobbies = availableLobbies;
+            this.isLoading = false;
+        });
 
-    // Listener for joining a game after backend confirmation
-    this.webSocketService.onNamespace<Lobby>(
-      SocketNamespace.Join,
-      JoinGameEvents.LobbyJoined,
-      (lobbyData) => this.router.navigate([this.routes.waitingRoom, lobbyData.lobbyId], { state: { lobby: lobbyData } }),
-    );
+        this.webSocketService.onNamespace<Lobby>(SocketNamespace.Join, JoinGameEvents.LobbyJoined, (lobbyData) =>
+            this.router.navigate([this.routes.waitingRoom, lobbyData.lobbyId], { state: { lobby: lobbyData } }),
+        );
 
-    // Emit event to get available lobbies on init.
-    this.webSocketService.emitNamespace(SocketNamespace.Join, JoinGameEvents.GetLobbies);
-  }
+        this.webSocketService.emitNamespace(SocketNamespace.Join, JoinGameEvents.GetLobbies);
+    }
 
-  ngOnDestroy(): void {
-    this.webSocketService.offNamespace(SocketNamespace.Join, JoinGameEvents.UpdatedLobbiesList);
-    this.webSocketService.offNamespace(SocketNamespace.Join, JoinGameEvents.LobbyJoined);
-  }
+    ngOnDestroy(): void {
+        this.webSocketService.offNamespace(SocketNamespace.Join, JoinGameEvents.UpdatedLobbiesList);
+        this.webSocketService.offNamespace(SocketNamespace.Join, JoinGameEvents.LobbyJoined);
+    }
 
-  selectLobby(lobby: Lobby): void {
-    this.router.navigate(['/character-selection', lobby.lobbyId], { state: { game: lobby.game } });
-  }
+    selectLobby(lobby: Lobby): void {
+        this.router.navigate(['/character-selection', lobby.lobbyId], { state: { game: lobby.game } });
+    }
 }
