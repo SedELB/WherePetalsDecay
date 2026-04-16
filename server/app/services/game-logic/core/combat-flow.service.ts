8b3a4aed7df9abc7b2ea40cf84f5c1521a2bb73b
SedELB <saadoun500@gmail.com>
2026-04-16 18:18:19 -0400

    fix: duplicate import

--- server/app/services/game-logic/core/combat-flow.service.ts
+++ server/app/services/game-logic/core/combat-flow.service.ts
@@ -17,8 +17,7 @@ import {
 } from '@common/interfaces/game-view';
 import { JoinGameEvents } from '@common/join.gateway.events';
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import type { Namespace, Socket } from 'socket.io';
-import { Server } from 'socket.io';
+import type { Namespace, Socket, Server } from 'socket.io';
 import { CombatStateService } from './combat-state.service';
 import { GameLogicService } from './game-logic.service';
 import { GameTurnSyncService } from './game-turn-sync.service';
