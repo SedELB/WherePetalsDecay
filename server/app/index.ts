58044a3ba1aeabb0c2448a4496fe9493b828eefd
SedELB <saadoun500@gmail.com>
2026-06-14 22:22:31 -0400

    fix: bind server to 0.0.0.0 for Railway routing

--- server/app/index.ts
+++ server/app/index.ts
@@ -18,7 +18,7 @@ const bootstrap = async () => {
     SwaggerModule.setup('api/docs', app, document);
     SwaggerModule.setup('', app, document);
 
-    await app.listen(process.env.PORT || 3000);
+    await app.listen(process.env.PORT || 3000, '0.0.0.0');
 };
 
 bootstrap();
